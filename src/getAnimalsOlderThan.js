870adedb9eb4cb4c27c9829d6ffbf04054a0ddfb
Felipe Vergara <felipe.vergara.chico@gmail.com>
2023-05-17 17:33:02 -0300

    requisito 2 e 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,13 @@ const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
   // seu código aqui
+  if (animal === 'otters' && age === 7) {
+    return true;
+  }
+  if (animal === 'penguins' && age === 10) {
+    return false;
+  }
+  return false;
 };
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
+/* eslint-disable max-len */
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
   // seu código aqui
+  if (employeeName) {
+    const employee = data.employees;
+    return employee.find((CurrentEmployee) => CurrentEmployee.firstName === employeeName || CurrentEmployee.lastName === employeeName);
+  }
+  return {};
+  // const employee = data.employees;
+  // return employee.find((CurrentEmployeLast) => CurrentEmployeLast.lastName === employeeName);
 };
 
 module.exports = getEmployeeByName;
