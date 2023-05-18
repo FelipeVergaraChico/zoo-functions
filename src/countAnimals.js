4b4e17d6911371d8863bee3ab20c060f0eff1286
Felipe Vergara <felipe.vergara.chico@gmail.com>
2023-05-18 17:09:41 -0300

    requisito 4

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,10 @@ const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
   // seu código aqui
+  const animals = data.species;
+  const finds = animals.find((name) => name.name === animal);
+  const counts = animals.map((f) => f.residents === finds);
+  return counts;
 };
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,20 @@ const data = require('../data/zoo_data');
 
 const isManager = (id) => {
   // seu código aqui
+
+  const pessoas = data.employees;
+  const gerentes = pessoas.filter((pessoa) => pessoa.managers.includes(id));
+  return gerentes.length > 0;
 };
 
 const getRelatedEmployees = (managerId) => {
   // seu código aqui
+  if (isManager(managerId)) {
+    const manager = data.employees;
+    const verify = manager.filter((manid) => manid.managers.includes(managerId));
+    return verify.map((employe) => `${employe.firstName} ${employe.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
 
 module.exports = { isManager, getRelatedEmployees };
