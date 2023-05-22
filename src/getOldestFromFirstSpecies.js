69499953097e00b00050476ddbe12d29604b887e
Felipe Vergara <felipe.vergara.chico@gmail.com>
2023-05-22 20:45:13 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,9 +1,19 @@
 const data = require('../data/zoo_data');
 
-// const { species, employees } = data;
+const { species, employees } = data;
 
 const getOldestFromFirstSpecies = (id) => {
   // seu código aqui
+  const idDoAnimalResponsavel = employees.find((funcionario) =>
+    funcionario.id === id).responsibleFor[0];
+  const animalFind = species.find((bichos) => bichos.id === idDoAnimalResponsavel).residents;
+  const older = animalFind.reduce((bicho1, bicho2) => {
+    if (bicho1.age > bicho2.age) {
+      return bicho1;
+    }
+    return bicho2;
+  });
+  return Object.values(older);
 };
 
 module.exports = getOldestFromFirstSpecies;
