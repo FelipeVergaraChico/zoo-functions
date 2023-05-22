78bbf51a53165d2b9b73662d0b48916409f8808f
Felipe Vergara <felipe.vergara.chico@gmail.com>
2023-05-22 18:48:50 -0300

    requisito 6,7 e 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,15 +1,39 @@
 const data = require('../data/zoo_data');
 
+const contaChildren = (entr) => {
+  const child = entr.filter((pessoa) => pessoa.age < 18);
+  return child.length;
+};
+const contaAdulto = (entr) => {
+  const adulto = entr.filter((pessoa) => pessoa.age >= 18 && pessoa.age < 50);
+  return adulto.length;
+};
+const contaSenhor = (entr) => {
+  const senhor = entr.filter((pessoa) => pessoa.age >= 50);
+  return senhor.length;
+};
 const countEntrants = (entrants) => {
   // seu código aqui
-  const obj = {};
-  const child = entrants.filter((c) => c.age < 18);
-  // const child = entrants.reduce((prev, elem) => prev + elem.age);
-  return child;
+  const child = contaChildren(entrants);
+  const adult = contaAdulto(entrants);
+  const senior = contaSenhor(entrants);
+  return { adult, child, senior };
 };
 
+const calculaAdulto = (preço) => data.prices.adult * preço;
+const calculaCrianca = (preço) => data.prices.child * preço;
+const calculaSenhor = (preço) => data.prices.senior * preço;
 const calculateEntry = (entrants) => {
   // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const count = countEntrants(entrants);
+  const adulto = calculaAdulto(count.adult);
+  const senhor = calculaSenhor(count.senior);
+  const criança = calculaCrianca(count.child);
+  const soma = (adulto + senhor + criança).toFixed(2);
+  return parseFloat(soma);
 };
 
 module.exports = { calculateEntry, countEntrants };
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,5 +1,25 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
-
+  test('Argumento count deve retornar a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  test('O argumento names deve retornar um array de nomes que possui o nome jefferson', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  test('O argumento averageAge deve retornar um número próximo a 10.5', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+  test('O argumento location deve retornar a string NW', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  test('O argumento popularity deve retornar um número igual ou maior a 5', () => {
+    expect(handlerElephants('popularity')).toBeLessThanOrEqual(5);
+  });
+  test('O argumento availability deve retornar um array de dias da semana que não contem Monday', () => {
+    expect(handlerElephants('availability')).not.toBe('Monday');
+  });
+  test('Quando não passado um parametro retorna undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
 });
