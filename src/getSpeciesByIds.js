a71f5b77f7c4669b493d9e59c7a7769eab2d77a0
Felipe Vergara <felipe.vergara.chico@gmail.com>
2023-05-22 20:20:06 -0300

    requisito 1 feito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
+const { species } = data;
+const getSpeciesByIds = (...ids) => {
   // seu código aqui.
+  if (!ids || !ids.length) {
+    return [];
+  }
+  if (Object.keys(ids).length === 1) {
+    return species.filter((animal) => ids.find((id) => animal.id === id));
+  }
+  return species.filter((animal) => ids.includes(animal.id));
 };
 
 module.exports = getSpeciesByIds;
