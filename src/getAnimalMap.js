5fb767e6f9afb7cd218835530f6ffebf3183d7f6
Felipe Vergara <felipe.vergara.chico@gmail.com>
2023-05-22 23:07:28 -0300

    requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
-const getAnimalMap = (options) => {
-  // seu código aqui
-};
+const { species } = data;
+
+function getAnimals(residents, sex, sorted) {
+  const animals = residents.filter((resident) =>
+    !sex || resident.sex === sex).map((resident) => resident.name);
+
+  if (sorted) {
+    animals.sort();
+  }
+
+  return animals;
+}
+
+function getAnimalMap(options = {}) {
+  const animalMap = { NE: [], NW: [], SE: [], SW: [] };
+
+  species.forEach(({ location, name, residents }) => {
+    const animalEntry = options.includeNames
+      ? { [name]: getAnimals(residents, options.sex, options.sorted) }
+      : name;
+
+    animalMap[location].push(animalEntry);
+  });
+
+  return animalMap;
+}
 
 module.exports = getAnimalMap;
